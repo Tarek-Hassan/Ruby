ada28b691ec82acf8e693cf156f623792e4c4926
Tarek-Hassan <tarek446240@gmail.com>
2020-04-25 23:40:42 +0200

    Lab2

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -1,17 +1,32 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new 
-      end
-      def create
-        @user = User.new(params[:user])
-        if @user.save
-          flash[:notice] = "You signed up successfully"
-          flash[:color]= "valid"
-        else
-          flash[:notice] = "Form is invalid"
-          flash[:color]= "invalid"
-        end
-        render "new"
-      end
+  def register
+    @user = User.new
+  end
+  def login
+    @user=User.find_user_by(user_params)
+    if @user != nil
+      redirect_to articles_path
+    else
+      render 'register'
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+  
+    if @user.save
+      redirect_to articles_path
+    else
+      render 'register'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+  
+
     
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- 
+  
+  # get 'welcome/index'
+  # get 'singup'
+root 'users#register'
+get '/register', to: 'users#register'
+post '/register/:user', to: 'users#create'
+get '/login', to: 'users#login'
+post '/login/:user', to: 'users#login'
+
+  # get 'welcome/index'
   resources :articles do
     resources:comments
   end
+
   
   # root to make ir defualt root
-  root 'welcome#index'
+  root 'users#new'
 end
